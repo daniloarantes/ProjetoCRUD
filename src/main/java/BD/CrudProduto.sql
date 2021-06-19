3d6852cca4c52d051a9adcb1802fd22c23f8aa43
Danilo Arantes <daniloarantes@gmail.com>
2021-06-18 21:28:17 -0300

    Adicionado script sql para criação do banco deste projeto

---
+++ src/main/java/BD/CrudProduto.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: database:3306
+-- Tempo de geração: 12/06/2021 às 17:02
+-- Versão do servidor: 5.7.34
+-- Versão do PHP: 7.4.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `CrudProduto`
+--
+CREATE DATABASE IF NOT EXISTS `CrudProduto` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `CrudProduto`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tbl_produto`
+--
+
+CREATE TABLE `tbl_produto` (
+  `id` int(11) NOT NULL,
+  `descricao` varchar(70) NOT NULL,
+  `valor` double NOT NULL,
+  `quantidade` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Despejando dados para a tabela `tbl_produto`
+--
+
+INSERT INTO `tbl_produto` (`id`, `descricao`, `valor`, `quantidade`) VALUES
+(1, 'Arroz ', 1, 2),
+(4, 'Feijão Vasconcelos', 8.5, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tbl_usuarios`
+--
+
+CREATE TABLE `tbl_usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `login` varchar(10) NOT NULL,
+  `senha` varchar(8) NOT NULL,
+  `tipo` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Despejando dados para a tabela `tbl_usuarios`
+--
+
+INSERT INTO `tbl_usuarios` (`id`, `nome`, `login`, `senha`, `tipo`) VALUES
+(2, 'Diego Ramos', 'diegor', '123456', 'ADM'),
+(3, 'teste', 'teste', '123', 'USR'),
+(4, 'Danilo Arantes', 'danilo', '123456', 'ADM');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `tbl_produto`
+--
+ALTER TABLE `tbl_produto`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `tbl_usuarios`
+--
+ALTER TABLE `tbl_usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `tbl_produto`
+--
+ALTER TABLE `tbl_produto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `tbl_usuarios`
+--
+ALTER TABLE `tbl_usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
